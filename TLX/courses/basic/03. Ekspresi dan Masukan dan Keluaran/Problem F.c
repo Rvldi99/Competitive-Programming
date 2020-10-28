fba01aec5abd4a6b1053b47af996b11dffacdc8b
Yuniar Rivaldi Nazar <54404794+Rvldi99@users.noreply.github.com>
2020-10-28 18:36:45 +0700

    Create Problem F.c

---
+++ TLX/courses/basic/03. Ekspresi dan Masukan dan Keluaran/Problem F.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d,e,f,g,h,i;
+    
+    scanf("%d %d %d", &a, &b, &c;
+    scanf("%d %d %d", &d, &e, &f;
+    scanf("%d %d %d", &g, &h, &i;
+    
+    printf("%d %d %d\n", a, b, c);
+    printf("%d %d %d\n", d, e, f);
+    printf("%d %d %d\n", g, h, i);
+}
