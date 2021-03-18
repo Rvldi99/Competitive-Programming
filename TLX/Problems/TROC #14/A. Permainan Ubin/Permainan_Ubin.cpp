b1bc26933df772ab5a1f6c00332fac430179e60d
Yuniar Rivaldi Nazar <54404794+Rvldi99@users.noreply.github.com>
2021-03-18 23:54:30 +0700

    Create Permainan_Ubin.cpp

---
+++ TLX/Problems/TROC #14/A. Permainan Ubin/Permainan_Ubin.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,m,hasil;
+
+    cin >> n;
+    cin >> m;
+
+    hasil = n * m;
+
+    if(hasil%2==0){
+        cout << hasil << endl;
+    }
+}
